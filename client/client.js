55482ffd285ae6649089eb0c041e13c63346ebec
Taha Manjoura <t.manjoura@gmail.com>
2022-08-31 16:25:29 -0500

    deploymentLab

---
+++ client/client.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('button');
+
+const alertMe = () => {
+    alert('Thanks for visiting my website!');
+};
+
+btn.addEventListener('click', alertMe);
---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//endpoints
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.htmls"))
+})
+
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/styles.css"))
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/client.js"))
+})
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
